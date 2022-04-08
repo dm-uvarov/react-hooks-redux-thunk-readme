fd8f5b2bee773bf832c354613e7126461a511342
Jimmy <dm.a.uvarov@gmail.com>
2022-04-08 15:57:57 -0400

    commit tree c6300f2e4a9d9847f5ac0e37ad2c28de9c57d11a

--- src/features/astronauts/Astronauts.js
+++ src/features/astronauts/Astronauts.js
@@ -7,15 +7,20 @@ function Astronauts() {
 
   const astronauts = useSelector((state) => state.astronauts.entities);
 
+  const isLoading = useSelector((state) => state.astronauts.status === "loading");
+
   function handleClick() {
     // dispatch the action creator (see below!)
     dispatch(fetchAstronauts());
   }
 
-  const astronautsList = astronauts.map((astro) => (
-    <li key={astro.id}>{astro.name}</li>
-  ));
 
+  if (isLoading) return <p>LOADING.. dont panic</p>;
+
+  const astronautsList = astronauts.map((astronaut) => (
+    <li key={astronaut.id}>{astronaut.name}</li>
+  ));
+  
   return (
     <div>
       <button onClick={handleClick}>Get Astronauts</button>
